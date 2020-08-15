edab64fb51390565eef0362a3839e07bc4a8f13e
jagrit19 <jagritlodha@gmail.com>
2020-08-15 19:42:19 +0530

    Minor Changes

--- Website/js/script.js
+++ Website/js/script.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
 	$('[data-toggle="tooltip"]').tooltip();
-	/*$('#NavButton').blur(function() {
-		$('#Navbar').collapse('hide');
-	});*/
 });
+
 $('#myCarousel').carousel( {interval: 3000} );
+
+/*if ($('#NavButton').blur() && $('.navbar').blur()) {
+	$('#Navbar').collapse('hide');
+}*/
+
+$('#NavButton').blur(function() {
+	$('#Navbar').collapse('hide');
+	console.log(this);
+});
