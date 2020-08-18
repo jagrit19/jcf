5d33e4e539d68b17558c27f8824da242448edc66
jagrit19 <jagritlodha@gmail.com>
2020-08-18 19:26:47 +0530

    Animations on scroll done

--- Website/js/script.js
+++ Website/js/script.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
 	$('[data-toggle="tooltip"]').tooltip();
+
+	$('.animate__animated').css('opacity', 0.2);
+
+  	$('#WhyUsPhoto').waypoint(function() {
+    	$('#WhyUsPhoto').addClass('animate__fadeIn');
+  	}, { offset: 'bottom-in-view' });
+
+  	$('#Reason1').waypoint(function() {
+    	$('#Reason1').addClass('animate__lightSpeedInLeft');
+    	$('#Reason1').css('opacity', 1);
+  	}, { offset: 'bottom-in-view' });
+  	
+  	$('#Reason2').waypoint(function() {
+    	$('#Reason2').addClass('animate__lightSpeedInRight');
+    	$('#Reason2').css('opacity', 1);
+  	}, { offset: 'bottom-in-view' });
+
+  	$('#Reason3').waypoint(function() {
+    	$('#Reason3').addClass('animate__lightSpeedInLeft');
+    	$('#Reason3').css('opacity', 1);
+  	}, { offset: 'bottom-in-view' });
+
+  	/*$('#Footer').waypoint(function() {
+    	$('#Footer').addClass('animate__fadeInUp');
+    	$('#Footer').css('opacity', 1!important);
+  	}, { offset: '0' });*/
 });
 
 $('#myCarousel').carousel( {interval: 3000} );
