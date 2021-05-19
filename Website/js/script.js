9668262ed1145d099c16b3ed09457a6c2acd6c67
jagrit19 <jagritlodha@gmail.com>
2021-05-19 17:53:26 +0530

    Final Changes done

--- Website/js/script.js
+++ Website/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  
 	$('[data-toggle="tooltip"]').tooltip();
 
 	$('.animate__animated').css('opacity', 0.2);
@@ -7,25 +8,46 @@ $(document).ready(function() {
     	$('#WhyUsPhoto').css('opacity', 1);
   	}, { offset: 'bottom-in-view' });*/
 
-  	$('#Reason1').waypoint(function() {
-    	$('#Reason1').addClass('animate__fadeInLeft');
+    $('#Section1Heading').waypoint(function() {
+      $('#Section1').addClass('animate__fadeInUp');
+      $('#Section1').css('opacity', 1);
+    }, { offset: 'bottom-in-view' });
+
+  	$('#Reason1Heading').waypoint(function() {
+    	$('#Reason1').addClass('animate__zoomInDown');
     	$('#Reason1').css('opacity', 1);
   	}, { offset: 'bottom-in-view' });
   	
-  	$('#Reason2').waypoint(function() {
-    	$('#Reason2').addClass('animate__fadeInRight');
+  	$('#Reason2Heading').waypoint(function() {
+    	$('#Reason2').addClass('animate__zoomInDown');
     	$('#Reason2').css('opacity', 1);
   	}, { offset: 'bottom-in-view' });
 
-  	$('#Reason3').waypoint(function() {
-    	$('#Reason3').addClass('animate__fadeInLeft');
+  	$('#Reason3Heading').waypoint(function() {
+    	$('#Reason3').addClass('animate__zoomInDown');
     	$('#Reason3').css('opacity', 1);
   	}, { offset: 'bottom-in-view' });
 
-  	/*$('#Footer').waypoint(function() {
-    	$('#Footer').addClass('animate__fadeInUp');
-    	$('#Footer').css('opacity', 1!important);
-  	}, { offset: '0' });*/
+    $('#Section2Heading').waypoint(function() {
+      $('#Section2').addClass('animate__fadeInUp');
+      $('#Section2').css('opacity', 1);
+    }, { offset: 'bottom-in-view' });
+
+    $('#Section3Heading').waypoint(function() {
+      $('#Section3').addClass('animate__fadeInUp');
+      $('#Section3').css('opacity', 1);
+    }, { offset: 'bottom-in-view' });
+
+    $('#Footer1Heading').waypoint(function() {
+      $('#Footer1').addClass('animate__fadeInUp');
+      $('#Footer1').css('opacity', 1);
+    }, { offset: 'bottom-in-view' });
+
+    $('#AboutUsHeading').waypoint(function() {
+      $('#AboutUsHeading').addClass('animate__fadeInDown');
+      $('#AboutUsHeading').css('opacity', 1);
+    }, { offset: 'bottom-in-view' });
+
 });
 
 $('#myCarousel').carousel( {interval: 3000} );
